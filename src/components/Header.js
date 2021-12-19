f6f87d122b1e6a8ed7b503eeae85f08fa7938419
kt1101 <khoatran.yuji1101@gmail.com>
2021-12-19 15:38:37 +0700

    login and signup UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ class Header extends Component {
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container">
 
-                    <Link to="/" className="navbar-brand logo">Shopthethao</Link>
+                    <Link to="/Home" className="navbar-brand logo">Shopthethao</Link>
 
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon" />
